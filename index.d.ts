89ca179636b9916add1e131ab82ddb3e7028ed00
Tameem Safi <tameem@safi.me.uk>
2018-09-22 13:53:24 +0100

    Added typescript typings

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+declare module "typewriter-effect" {
+  export interface Options {
+    strings: ?Array<string>,
+    cursor: string,
+    delay: 'natural' | number,
+    deleteSpeed: 'natural' | number,
+    loop: boolean,
+    autoStart: boolean,
+    devMode: boolean,
+    wrapperClassName: string,
+    cursorClassName: string,
+  }
+
+  class Typewriter {
+    constructor(container: string | HTMLElement, options: Options);
+
+    start(): Typewriter;
+    stop(): Typewriter;
+    pause(): Typewriter;
+    pauseFor(ms: number): Typewriter;
+    typeString(string: string): Typewriter;
+    deleteAll(speed?: 'natural' | number): Typewriter;
+    changeDeleteSpeed(speed: number): Typewriter;
+    changeDelay(delay: number): Typewriter;
+    deleteChars(amount: number): Typewriter;
+    callFunction(callback: Function, thisArg: Object): Typewriter;
+  }
+
+  export default Typewriter;
+}
