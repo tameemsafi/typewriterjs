5b9ac7b74a4282cb8d1b979ecd7fc1b9bd347ff5
Johnny Mikhael <johnny.mickael@gmail.com>
2020-05-11 09:48:23 +0200

    Added Vue Component

---
+++ src/vue/Typewriter.vue
@@ -0,0 +1,32 @@
+<template>
+  <span></span>
+</template>
+<script>
+  import Typewriter from 'typewriter-effect/dist/core';
+  export default {
+    props: {
+      strings: {
+        type: Array,
+        required: true,
+        default: () => []
+      },
+      config: {
+        type: Object,
+        required: false,
+        default: () => {}
+      }
+    },
+    data() {
+      return {
+        instance: null,
+      }
+    },
+    mounted() {
+      const newConfig = Object.assign({strings: this.strings},{autoStart: true, loop: true}, this.config);
+      this.instance = new Typewriter(this.$el, newConfig);
+    },
+    destroyed() {
+      this.instance.stop();
+    }
+  }
+</script>
